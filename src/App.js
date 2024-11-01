ce5b8e9f08b1eddade91eb94a6cedc81d91244f2
BastyW <bastyelj2005@gmail.com>
2024-10-31 21:13:57 -0300

    Guardado de datos de forma local.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import { HotTable, HotColumn } from "@handsontable/react";
 import { registerAllModules } from 'handsontable/registry';
+
 import "handsontable/dist/handsontable.full.css";
 import {registerLanguageDictionary, esMX} from "handsontable/i18n"
 
@@ -12,36 +13,78 @@ registerAllModules();
 registerLanguageDictionary(esMX);
 
 function App() {
-  const [usuarios, setUsuarios] = React.useState([]);
+
+  const datosLocales = [
+    {
+      id: 1,
+      name: "Juan Pérez",
+      username: "juanp",
+      email: "juanp@example.com",
+      address: { street: "Calle 1", city: "Ciudad 1" },
+    },
+    {
+      id: 2,
+      name: "Ana García",
+      username: "anag",
+      email: "anag@example.com",
+      address: { street: "Calle 2", city: "Ciudad 2" },
+    },
+    {
+      id: 3,
+      name: "Luis Rodríguez",
+      username: "luisr",
+      email: "luisr@example.com",
+      address: { street: "Calle 3", city: "Ciudad 3" },
+    },
+
+  ];
 
   const hotTableComponent = React.useRef(null);
 
+  const cargarDatos = () => {
+    const datosGuardados = localStorage.getItem("usuarios");
+    return datosGuardados ? JSON.parse(datosGuardados) : datosLocales;
+  };
+  const [usuarios, setUsuarios] = React.useState(cargarDatos);
 
   React.useEffect(() => {
-    function getData(){
-      fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => setUsuarios(data))
-    }
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  }, [usuarios]);
 
-    getData();
-  }, []);
+  const handleAfterChange = (changes) => {
+    if (changes) {
+      const nuevosUsuarios = [...usuarios];
+      changes.forEach(([row, prop, oldValue, newValue]) => {
+        const keys = prop.split(".");
+        let target = nuevosUsuarios[row];
+
+        for (let i = 0; i < keys.length - 1; i++) {
+          target = target[keys[i]];
+        }
+
+        target[keys[keys.length - 1]] = newValue;
+      });
+      setUsuarios(nuevosUsuarios);
+    }
+  };
 
   const descargarArchivo = () => {
-    const pluginDescarga =
-      hotTableComponent.current.hotInstance.getPlugin("exportFile");
-
+    const pluginDescarga = hotTableComponent.current.hotInstance.getPlugin("exportFile");
+  
     pluginDescarga.downloadFile("csv", {
       filename: "usuarios",
       fileExtension: "csv",
       mimeType: "text/csv",
       columnHeaders: true,
+      columnDelimiter: ";", // Punto y coma para separar por columnas ;VVVV
+      rowHeaders: false,
     });
   };
-
+  
+  
   return (
     <div>
-      <h2>Hola, gente</h2>
+      <h2>Hola, mundo</h2>
       <button onClick={() => descargarArchivo()}>Descargar archivo</button>
 
       {usuarios && 
@@ -50,8 +93,13 @@ function App() {
           rowHeaders={true}
           columnSorting={true}
           mergeCells={true}
-          // contextMenu={true}
-          contextMenu={["row_above", "row_below"]}
+          manualColumnMove={true}
+          manualColumnResize={true}
+          autoWrapRow= {true}
+          autoWrapCol= {true}
+          afterChange={handleAfterChange}
+           contextMenu={true}
+          //contextMenu={["row_above", "row_below", "col_left", "col_right", "remove_row", "remove_col"]}
           // readOnly={true}
           >
           
